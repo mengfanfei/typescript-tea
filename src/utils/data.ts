92acdc370d890e7f43e19cbc4c5d488279ef0b6d
mengfanfei <mengfanfei@zcunsoft.com>
2020-06-11 14:48:16 +0800

    细小修改

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -47,12 +47,12 @@ export const todoListData: Todo[] = [
     isCompleted: false
   },
   {
-    id: "5",
+    id: "4",
     content: "图雀社区：汇聚精彩的免费实战教程",
-    user: "12345678",
+    user: "666666666",
     date: "2020年3月2日 19:34",
     isCompleted: false
-  }
+  },
 ]
 
 export const userList: User[] = [
@@ -70,11 +70,6 @@ export const userList: User[] = [
     id: "25455350",
     name: "crxk",
     avatar: "https://avatars1.githubusercontent.com/u/25455350?s=96&v=4"
-  },
-  {
-    id: "23410977",
-    name: "pftom",
-    avatar: "https://avatars0.githubusercontent.com/u/23410977?s=96&v=4"
   },
   {
     id: "58352313",
